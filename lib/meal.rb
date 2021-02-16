cc45d740a639bf16538a1edef4bdfbee860815a2
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-16 18:16:41 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,4 +1,14 @@
 require 'pry'
 
 class Meal
+  attr_accessor :waiter, :customer, :total, :tip
+
+  @@all = []
+
+  def initialize(waiter, customer, total, tip = 0)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+  end
 end
