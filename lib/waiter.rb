8a7d01c5fb4ed515e4d164d1529a11665a68c241
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-16 18:14:09 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,4 +1,13 @@
 require 'pry'
 
 class Waiter
+  attr_accessor :name, :yrs_experience
+
+  @@all = []
+
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
 end
